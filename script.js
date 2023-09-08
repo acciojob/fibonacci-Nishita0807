5d0e5356a131236f44deeee394b0701e9029438d
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-08 15:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-// your code here
+  if (num <= 0) {
+        return 0; // First term of the Fibonacci sequence
+    } else if (num === 1) {
+        return 1; // Second term of the Fibonacci sequence
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+	
 }
 
 module.exports = fibonacci;
